bef9cba6394dee69367d1db385e9bb7a4b4aba0f
Eylon Shmilovich <eylonshm@gmail.com>
2023-07-26 19:39:37 +0300

    Add Sprites constructors

---
+++ src/sprites/Ball.ts
@@ -0,0 +1,16 @@
+import { Vector } from '../constants'
+
+export class Ball {
+  private image: HTMLImageElement = new Image()
+  constructor(
+    private size: number,
+    private position: Vector,
+    private speed: number,
+    image: string,
+  ) {
+    this.size = size
+    this.position = position
+    this.speed = speed
+    this.image.src = image
+  }
+}
